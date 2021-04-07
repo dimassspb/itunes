b5eb030689bf51ae9b8d107bff56efcd4f3d2aba
Dmitrii Isakov <dimass82@mail.ru>
2021-04-07 10:56:45 +0300

    radioPlayer added

--- scripts/radioPlayer.js
+++ scripts/radioPlayer.js
@@ -3,7 +3,7 @@ export const radioPlayerInit = () => {
   const radioNavigation = document.querySelector('.radio-navigation');
   const radioCoverImg = document.querySelector('.radio-cover__img');
   const radioItem = document.querySelectorAll('.radio-item');
-  const radioHeader = document.querySelector('.radio-header');
+  const radioHeaderBig = document.querySelector('.radio-header__big');
   const radioStop = document.querySelector('.radio-stop');
 
   const audio = new Audio();
@@ -13,20 +13,36 @@ export const radioPlayerInit = () => {
 
   const changeIconPlay = () => {
     if (audio.paused) {
+      radio.classList.remove('play');
       radioStop.classList.add('fa-play');
       radioStop.classList.remove('fa-stop');
     } else {
+      radio.classList.add('play');
       radioStop.classList.add('fa-stop');
       radioStop.classList.remove('fa-play');
     }
   };
 
+  const selectItem = (elem) => {
+    radioItem.forEach((item) => item.classList.remove('select'));
+    elem.classList.add('select');
+  };
+
   radioNavigation.addEventListener('change', (event) => {
     const target = event.target;
+    const parrent = target.closest('.radio-item');
+    selectItem(parrent);
+
+    const title = parrent.querySelector('.radio-name').textContent;
+    radioHeaderBig.textContent = title;
+
+    const urlImg = parrent.querySelector('.radio-img').src;
+    radioCoverImg.src = urlImg;
+
     radioStop.disabled = false;
     audio.src = target.dataset.radioStantion;
     audio.play();
-    changeIconPlay()
+    changeIconPlay();
   });
 
   radioStop.addEventListener('click', () => {
